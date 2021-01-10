7080d29882493fcb19351846111655cdaf7582cd
James Scott-Brown <james@jamesscottbrown.com>
2021-01-10 12:33:27 +0000

    remove commented-out test cod

--- src/lineSegments.test.js
+++ src/lineSegments.test.js
@@ -57,34 +57,3 @@ test('minspacing works', () => {
     expect(checkSpaces(respacedData)).toBeTruthy();
     expect(respacedData.length).toBe(data.length);
 });
-
-
-/*
-test('check effect of increasing distance threshold', () => {
-    const data = [{x: 4}, {x: 5}, {x: 2}, {x: 9}, {x: 5}];
-
-    const respacedData = repositionLineSegments(data, 1000, 10);
-    expect(checkOrdered(respacedData)).toBeTruthy();
-    expect(checkSpaces(respacedData, 10)).toBeTruthy();
-    expect(checkSpaces(respacedData, 100)).toBeFalsy();
-
-
-    const respacedData2 = repositionLineSegments(data, 1000, 100);
-    expect(checkOrdered(respacedData2)).toBeTruthy();
-    expect(checkSpaces(respacedData2, 100)).toBeTruthy();
-});
-
-test('runs', () => {
-    const data = [{pos: 4}, {pos: 5}, {pos: 2}, {pos: 9}, {pos: 5}]
-
-    // fails if oldPositionName argument not provided
-    const respacedData = repositionLineSegments(data, 1000, 10);
-    const sumPos = respacedData.map(d => d.newX).reduce((sum,x) => sum + x);
-    expect(sumPos).toBeNaN()
-
-    // succeeds if oldPositionName argument is provided
-    const respacedData2 = repositionLineSegments(data, 1000, 10, "pos");
-    expect(checkOrdered(respacedData2)).toBeTruthy();
-    expect(checkSpaces(respacedData2)).toBeTruthy();
-});
-*/
